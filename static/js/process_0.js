04424e182dcca6f4f66280743baac0961b1349cb
Ohashi_uhbie <tmatomatox1@gmail.com>
2022-03-12 13:35:15 +0900

    名前登録コマンド 実装

--- static/js/process_0.js
+++ static/js/process_0.js
@@ -1,20 +1,55 @@
-var stored_name = localStorage.getItem("name_CUICUI");
-var user_name = "Unknown";
-
 $(() => {
-	append_input_div();
+	if(localStorage.getItem("name_CUICUI") == null){
+		p0_respond("\"助けて\" をタイプしてコマンドを表示");
+	}else{
+		p0_respond("ようこそ、" + localStorage.getItem("name_CUICUI"));
+	}
 });
-
+const help_text = [
+	[
+		"名前 `名前`: ユーザーネームを`名前`に設定",
+		"開く `名前`: 対戦インスタンス`名前`を作成",
+		"開く `名前` `password`: パーワード付きで作成",
+		"一覧: 現在開いている対戦インスタンスを表示"
+	]
+];
 function process_0(input){
-	console.log("process_0:");
-	console.log(input);
-
-	append_input_div();
+	split_input = input.split(" ");
+	console.log("process_0:" + split_input);
+	if(split_input[0] == "助けて"){
+		var help_text = dump_help(0);
+		p0_respond(help_text);
+	}else if(split_input[0] == "名前"){
+		console.log(split_input[1]);
+		if(split_input[1] !== undefined && split_input[1] !== ""){
+			localStorage.setItem("name_CUICUI", split_input[1]);
+			p0_respond("お名前が登録されました。");
+		}else{
+			p0_respond("第2引数にお名前を入力してください。")
+		}
+	}else if(split_input[0] == "rem"){
+		localStorage.removeItem("name_CUICUI");
+		p0_respond("removed name");
+	}else{
+		p0_respond("");
+	}
+}
+function dump_help(which){
+	var help = help_text[which];
+	var res = "<ul>";
+	help.forEach((x) => {
+		res += "<li>" + x + "</li>";
+	});
+	res += "</ul>";
+	return res;
 }
  
-function append_input_div(){
+function p0_respond(arg_text){
+	var response_div = $('<div>'+arg_text+'</div>');
 	var input_div = $('<div>');
-	var input_div_name = $('<span>', {class:'green-name', text:user_name });
+	var stored_name = localStorage.getItem("name_CUICUI");
+	if(stored_name == null){stored_name = "Unknown";}
+	var input_div_name = $('<span>', {class:'green-name', text: stored_name});
 	var input_div_input = $('<input>', {class: 'user-input input-ue'})
 	input_div_input.keydown((e) => {
 		if(e.key == "Enter"){
@@ -26,6 +61,7 @@ function append_input_div(){
 	input_div.append(": ");
 	input_div.append(input_div_input);
 
+	$("#p2").append(response_div);
 	$("#p2").append(input_div);
 	input_div_input.focus();
 }
