51cd54480d4b110d27adc0aac86d0b86a5729c34
Ohashi_uhbie <tmatomatox1@gmail.com>
2022-03-12 12:26:57 +0900

    コマンド入力ゾーンの追加に成功した。ちゃんとkeydownのenter時の処理なども動いている。tabによるカーソル移動も完璧だ。

--- static/js/input.js
+++ static/js/input.js
@@ -1,47 +1,53 @@
 var input_switch = false;
 var elms_input = [
-	$(".user-input").eq(0),
-	$(".user-input").eq(1)
+	$(".input-ue"),
+	$(".input-shita")
 ];
 $(() => {
-	elms_input[Number(input_switch)].focus();
+	elms_input[Number(input_switch)].last().focus();
 });
+////////////////////////////////////////
 $(document).keydown(function(e){
 	//console.log("key: " + e.key + "(len:"+(e.key).length+")"); 
 	if(e.key == "Tab"){
 		input_switch = !input_switch;
-		elms_input[Number(input_switch)].focus();
+		elms_input[Number(input_switch)].last().focus();
 		return false;
 	}
 	if(e.key == "Escape"){
 		console.log("sync!")
 	}
 });
-elms_input[0].click((e)=>{
+$(document).keyup(function(e){
+	elms_input = [
+		$(".input-ue"),
+		$(".input-shita")
+	];
+});
+elms_input[0].last().click((e)=>{
 	input_switch = false;
 });
-elms_input[1].click((e)=>{
+elms_input[1].last().click((e)=>{
 	input_switch = true;
 });
+
 
 ////////////////////////////////////////////
 elms_input[0].keydown((e) => {
 	if(e.key == "Enter"){
-		var input = elms_input[0].val();
-		elms_input[0].val("");
-		//var input = $(this).eq(0).val();
+		var input = elms_input[0].last().val();
 		process_0(input);
 	}
 });
 elms_input[1].keydown((e) => {
 	if(e.ctrlKey == true && e.key == "Enter"){
-		var input = elms_input[1].val();
-		elms_input[1].val("");
+		var input = elms_input[1].last().val();
+		elms_input[1].last().val("");
 		//var input = $(this).eq(0).val();
 		process_1(input);
 	}
 });
 elms_input[1].keyup((e) => {
-	var input = elms_input[1].val();
+	var input = elms_input[1].last().val();
 	console.log("途中: "+ input);
 });
--- static/js/process_0.js
+++ static/js/process_0.js
@@ -1,6 +1,10 @@
 var stored_name = localStorage.getItem("name_CUICUI");
 var user_name = "Unknown";
 
+$(() => {
+	append_input_div();
+});
+
 function process_0(input){
 	console.log("process_0:");
 	console.log(input);
@@ -11,7 +15,13 @@ function process_0(input){
 function append_input_div(){
 	var input_div = $('<div>');
 	var input_div_name = $('<span>', {class:'green-name', text:user_name });
-	var input_div_input = $('<input>', {class: 'user-input'})
+	var input_div_input = $('<input>', {class: 'user-input input-ue'})
+	input_div_input.keydown((e) => {
+		if(e.key == "Enter"){
+			var input = elms_input[0].last().val();
+			process_0(input);
+		}
+	});
 	input_div.append(input_div_name);
 	input_div.append(": ");
 	input_div.append(input_div_input);
